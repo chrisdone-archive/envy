42f32050d87d9e3b63ef6288b3ba565ea7f2d263
chris <chris@chriss-MacBook-Pro.local>
2022-03-16 17:29:09 +0000

    Working

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # envy
+
+**Works for M1 Apple only.**
+
+Go to a terminal using direnv or nix, run:
+
+    envy save foo
+
+Where foo is the name of your project. Now you can run this elsewhere
+(e.g. Emacs):
+
+    envy exec foo blah
+
+It will execute `blah` with the environment from your other terminal.
+
+When direnv/nix changes, just re-run the save command.
--- app/Main.hs
+++ app/Main.hs
@@ -1,5 +1,6 @@
 module Main where
 
+import System.Directory
 import System.Environment (getEnv, getEnvironment, getArgs)
 import System.Process.Typed
 
@@ -8,10 +9,12 @@ main = do
   args <- getArgs
   home <- getEnv "HOME"
   case args of
-    [] -> do
-      putStrLn "Writing environment to ~/.envy"
+    ["save", name] -> do
+      createDirectoryIfMissing True (home <> "/.envy/")
+      putStrLn ("Saving environment to ~/.envy/" <> name)
       env <- getEnvironment
-      writeFile (home <> "/.envy") (show env)
-    cmd:args -> do
-      env <- fmap read (readFile (home <> "/.envy"))
+      writeFile (home <> "/.envy/" <> name) (show env)
+    ("exec":name:cmd:args) -> do
+      env <- fmap read (readFile (home <> "/.envy/" <> name))
       runProcess_ (setEnv env (proc "arch" ("-x86_64":cmd:args)))
+    _ -> error "Args:\n\nsave NAME\n\nexec NAME CMD ARG1 ARG_N.."
