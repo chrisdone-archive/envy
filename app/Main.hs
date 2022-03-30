d975ebfe19517c8cf9790857c8e4752fb3c9f7bb
Chris Done <chris.done@artificial.io>
2022-03-30 10:19:06 +0000

    Inherit the env provided by the file

--- app/Main.hs
+++ app/Main.hs
@@ -2,6 +2,7 @@ module Main where
 
 import System.Directory
 import System.Environment (getEnv, getEnvironment, getArgs)
+import qualified System.Environment as Env
 import System.Process.Typed
 
 main :: IO ()
@@ -16,5 +17,6 @@ main = do
       writeFile (home <> "/.envy/" <> name) (show env)
     ("exec":name:cmd:args) -> do
       env <- fmap read (readFile (home <> "/.envy/" <> name))
+      maybe (pure ()) (Env.setEnv "PATH") (lookup "PATH" env)
       runProcess_ (setEnv env (proc cmd args))
     _ -> error "Args:\n\nsave NAME\n\nexec NAME CMD ARG1 ARG_N.."
